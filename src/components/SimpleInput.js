11f31545beb14c55e71c5026324061d6a85a647f
Aman Gupta <amanguptaprogrammer@gmail.com>
2022-08-21 17:10:27 +0530

    form elements by using UseRef & useState

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,9 +1,33 @@
+import { useRef, useState } from "react";
+
 const SimpleInput = (props) => {
+  const nameInputRef = useRef();
+  const [enteredName, setEnteredName] = useState("");
+
+  const nameInputChangeHandler = (event) => {
+    setEnteredName(event.target.value);
+  };
+
+  const formSumbitHandler = (event) => {
+    event.preventDefault();
+    const enteredValue = nameInputRef.current.value;
+    console.log(enteredValue);
+    props.onSubmit(enteredName);
+    // nameInputRef.current.value = ""; => Not ideal because we are using useRef() which manipulates the DOM directly.
+    setEnteredName("");
+  };
+
   return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+    <form onSubmit={formSumbitHandler}>
+      <div className="form-control">
+        <label htmlFor="name">Your Name</label>
+        <input
+          ref={nameInputRef}
+          type="text"
+          id="name"
+          value={enteredName}
+          onChange={nameInputChangeHandler}
+        />
       </div>
       <div className="form-actions">
         <button>Submit</button>
